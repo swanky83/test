f266aedcd5fb2d248cd14159ebbd61aa050ab7b8
Michał Piaskowski / Swanky <michal.piaskowski@gmail.com>
2017-01-28 12:04:29 +0100

    test2

--- web.go
+++ web.go
@@ -1,137 +1,126 @@
-/*
-Copyright 2014 Google Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// outyet is a web server that announces whether or not a particular Go version
-// has been tagged.
 package main
-
+ 
 import (
-    "expvar"
-    "flag"
-    "fmt"
-    "html/template"
-    "log"
     "net/http"
-    "sync"
+    "fmt"
+    "strings"
+    "encoding/json"
+    "os"
+    "github.com/guillermo/go.procmeminfo"
+    "syscall"
+    "io/ioutil"
+    "strconv"
     "time"
 )
-
-// Command-line flags.
-var (
-    httpAddr   = flag.String("http", ":8080", "Listen address")
-    pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
-    version    = flag.String("version", "1.4", "Go version")
+type DiskStatus struct {
+    All  uint64 `json:"all"`
+    Used uint64 `json:"used"`
+    Free uint64 `json:"free"`
+}
+ 
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+    fs := syscall.Statfs_t{}
+    err := syscall.Statfs(path, &fs)
+    if err != nil {
+        return
+    }
+    disk.All = fs.Blocks * uint64(fs.Bsize)
+    disk.Free = fs.Bfree * uint64(fs.Bsize)
+    disk.Used = disk.All - disk.Free
+    return
+}
+ 
+const (
+    B  = 1
+    KB = 1024 * B
+    MB = 1024 * KB
+    GB = 1024 * MB
 )
-
-const baseChangeURL = "https://go.googlesource.com/go/+/"
-
+ 
+func getCPUavg() (idle, total uint64) {
+    contents, err := ioutil.ReadFile("/proc/stat")
+    if err != nil {
+        return
+    }
+    lines := strings.Split(string(contents), "\n")
+    for _, line := range(lines) {
+        fields := strings.Fields(line)
+        if fields[0] == "cpu" {
+            numFields := len(fields)
+            for i := 1; i < numFields; i++ {
+                val, err := strconv.ParseUint(fields[i], 10, 64)
+                if err != nil {
+                    fmt.Println("Error: ", i, fields[i], err)
+                }
+                total += val // tally up all the numbers to get total ticks
+                if i == 4 {  // idle is the 5th field in the cpu line
+                    idle = val
+                }
+            }
+            return
+        }
+    }
+    return
+}
+ 
+type ServerStatus struct {
+    Hostname  string `json:"hostname"`
+    CPUAvg    float64 `json:"cpu_avg"`
+    DiskUsage float64 `json:"disk_usage"`
+    MemUsage  uint64 `json:"mem_usage"`
+    CheckTime string `json:"check_time"`
+}
+ 
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+    name, err := os.Hostname()
+ 
+    if err != nil {
+        panic(err)
+    }
+ 
+    meminfo := &procmeminfo.MemInfo{}
+    meminfo.Update()
+    usage_memory := meminfo.Used()
+    if usage_memory > 0 {
+        usage_memory = usage_memory / 1024 /1024
+    }
+    disk := DiskUsage("/")
+    free_space := float64(disk.Free)/float64(GB)
+ 
+ 
+    idle0, total0 := getCPUavg()
+    time.Sleep(3 * time.Second)
+    idle1, total1 := getCPUavg()
+ 
+    idleTicks := float64(idle1 - idle0)
+    totalTicks := float64(total1 - total0)
+    cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+ 
+ 
+    sstatus := ServerStatus{name, cpuUsage, free_space, usage_memory, "Alex5"}
+ 
+    js, err := json.Marshal(sstatus)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+ 
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(js)
+}
+ 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    remPartOfURL := r.URL.Path[len("/hello/"):] //get everything after the /hello/ part of the URL
+    fmt.Fprintf(w, "Hello %s!", remPartOfURL)
+}
+ 
+func shouthelloHandler(w http.ResponseWriter, r *http.Request) {
+    remPartOfURL := r.URL.Path[len("/shouthello/"):] //get everything after the /shouthello/ part of the URL
+    fmt.Fprintf(w, "Hello %s!", strings.ToUpper(remPartOfURL))
+}
+ 
 func main() {
-    flag.Parse()
-    changeURL := fmt.Sprintf("%sgo%s", baseChangeURL, *version)
-    http.Handle("/", NewServer(*version, changeURL, *pollPeriod))
-    log.Fatal(http.ListenAndServe(*httpAddr, nil))
+    http.HandleFunc("/status", statusHandler)
+    http.ListenAndServe("0.0.0.0:9999", nil)
 }
-
-// Exported variables for monitoring the server.
-// These are exported via HTTP as a JSON object at /debug/vars.
-var (
-    hitCount       = expvar.NewInt("hitCount")
-    pollCount      = expvar.NewInt("pollCount")
-    pollError      = expvar.NewString("pollError")
-    pollErrorCount = expvar.NewInt("pollErrorCount")
-)
-
-// Server implements the outyet server.
-// It serves the user interface (it's an http.Handler)
-// and polls the remote repository for changes.
-type Server struct {
-    version string
-    url     string
-    period  time.Duration
-
-    mu  sync.RWMutex // protects the yes variable
-    yes bool
-}
-
-// NewServer returns an initialized outyet server.
-func NewServer(version, url string, period time.Duration) *Server {
-    s := &Server{version: version, url: url, period: period}
-    go s.poll()
-    return s
-}
-
-// poll polls the change URL for the specified period until the tag exists.
-// Then it sets the Server's yes field true and exits.
-func (s *Server) poll() {
-    for !isTagged(s.url) {
-	pollSleep(s.period)
-    }
-    s.mu.Lock()
-    s.yes = true
-    s.mu.Unlock()
-    pollDone()
-}
-
-// Hooks that may be overridden for integration tests.
-var (
-    pollSleep = time.Sleep
-    pollDone  = func() {}
-)
-
-// isTagged makes an HTTP HEAD request to the given URL and reports whether it
-// returned a 200 OK response.
-func isTagged(url string) bool {
-    pollCount.Add(1)
-    r, err := http.Head(url)
-    if err != nil {
-	log.Print(err)
-	pollError.Set(err.Error())
-	pollErrorCount.Add(1)
-	return false
-    }
-    return r.StatusCode == http.StatusOK
-}
-
-// ServeHTTP implements the HTTP user interface.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    hitCount.Add(1)
-    s.mu.RLock()
-    data := struct {
-	URL     string
-	Version string
-	Yes     bool
-    }{
-	s.url,
-	s.version,
-	s.yes,
-    }
-    s.mu.RUnlock()
-    err := tmpl.Execute(w, data)
-    if err != nil {
-	log.Print(err)
-    }
-}
-
-// tmpl is the HTML template that drives the user interface.
-var tmpl = template.Must(template.New("tmpl").Parse(`
-<!DOCTYPE html><html><body><center>
-    <h2>Is Go {{.Version}} out yet?</h2>
-    <h1>
-    {{if .Yes}}
-	<a href="{{.URL}}">YES!</a>
-    {{else}}
-	No. :-(
-    {{end}}
-    </h1>
-</center></body></html>
-`))
